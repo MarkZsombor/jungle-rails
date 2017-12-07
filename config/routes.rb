aa94086a9ccecab5d42314523f2714f85fa8158e
MarkZsombor <markszsombor@gmail.com>
2017-12-07 20:17:07 +0000

    added sessions controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
-
   resources :users, only: [:create, :new]
+  resources :sessions, only: [:new, :create, :destroy]
 
   namespace :admin do
     root to: 'dashboard#show'
