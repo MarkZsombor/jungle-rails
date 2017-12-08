882be7760342ff1ff6950003f36ca128f970e130
MarkZsombor <markszsombor@gmail.com>
2017-12-08 22:11:36 +0000

    user can now add reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,31 @@
 class ReviewsController < ApplicationController
+  before_action :set_review, only: [:destroy]
 
   def create
+    @review = Review.new(review_params)
+    @product = Product.find(params[:product_id])
+    @review.product = @product
+    @review.user = current_user
 
+    if @review.save!
+      redirect_to @product, notice: 'Thank you for rating this product'
+    else
+      redirect_to @product
+    end
   end
 
   def destroy
+    @review.destroy
+    redirect_to @review.product, notice: 'Review deleted'
   end
 
+  private
+
+    def set_review
+      @review = Review.find(params[:id])
+    end
+
+    def review_params
+      params.require(:review).permit(:description, :rating)
+    end
 end
