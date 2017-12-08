4c21ed3f0f8d8a32ccdeed60f3e04b3bae56cad5
MarkZsombor <markszsombor@gmail.com>
2017-12-08 19:26:40 +0000

    added 1-5 values validation for review ratings

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,5 +5,6 @@ class Review < ActiveRecord::Base
   validates :product_id, presence: true
   validates :user_id, presence: true
   validates :rating, presence: true
+  validates_inclusion_of :rating, :in => 1..5
 
 end
