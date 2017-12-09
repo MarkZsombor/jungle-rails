6bb671c0e0825d8de91a7dd56f94c1af24fdec68
MarkZsombor <markszsombor@gmail.com>
2017-12-09 01:26:16 +0000

    styled reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,4 +7,8 @@ class Review < ActiveRecord::Base
   validates :user_id, presence: true
   validates :rating, presence: true
   validates_numericality_of :rating, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 5
+
+  def blank_stars
+    5 - rating.to_i
+  end
 end
